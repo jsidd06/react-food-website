157c7d8f760d0f5b3fdca1974466d5163371211c
siddharth <jsidd06@gmail.com>
2023-02-17 17:21:33 +0530

    already update item check

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,9 +8,24 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updateItems = state.items.concat(action.item);
     const updateTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const alreadyHasCartItem = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItems = state.items[alreadyHasCartItem];
+    let updateItems;
+    if (existingCartItems) {
+      const updateItem = {
+        ...existingCartItems,
+        amount: existingCartItems.amount + action.item.amount,
+      };
+      updateItems = [...state.items];
+      updateItems[alreadyHasCartItem] = updateItem;
+    } else {
+      updateItems = state.items.concat(action.item);
+    }
     return {
       items: updateItems,
       totalAmount: updateTotalAmount,
