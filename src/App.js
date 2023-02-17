3b5c42b886317d98c39d3d7f2831e103a096c292
siddharth <jsidd06@gmail.com>
2023-02-17 11:14:25 +0530

    cart modal backdrop portal funcationality added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import Cart from "./components/Cart";
 import Header from "./Layout/Header";
 import Meals from "./Meals/Meals";
 
 function App() {
+  const [cartIsShow, setCartIsShow] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShow(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShow(false);
+  };
+
   return (
     <div>
-      <Header />
+      {cartIsShow && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -3,12 +3,12 @@ import Image from "../Images/veg.jpg";
 import classes from "./Header.module.css";
 import HeaderCardButton from "./HeaderCardButton";
 
-function Header() {
+function Header(props) {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>Food Meals</h1>
-        <HeaderCardButton />
+        <h1>Food store</h1>
+        <HeaderCardButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={Image} alt="food" />
